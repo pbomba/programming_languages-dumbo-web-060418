bb4d4f144d6f74db05557d66dde34829fbb74b29
Paul Bomba <paul@paulbomba.com>
2018-06-04 00:37:19 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,20 +8,20 @@ newHash = {}
       newHash[lang] = type
     end
   end
-styleArray = []
-  languages.each do |family, language|
-    language.each do |lang, type|
-      styleArray << family
-    end
-  end
+#styleArray = []
+#  languages.each do |family, language|
+#    language.each do |lang, type|
+#      styleArray << family
+#    end
+#  end
+#
+#i = 0
+#
+#  newHash.each do |lang, infoz|
+#    infoz[:style] = [styleArray[i]]
+#    i += 1
+#  end
 
-i = 0
-
-  newHash.each do |lang, infoz|
-    infoz[:style] = [styleArray[i]]
-    i += 1
-  end
-
-return newHash
+puts newHash
 end
 
