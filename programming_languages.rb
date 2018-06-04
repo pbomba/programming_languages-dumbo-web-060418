64b0c5aab02f1be2828ed643676c64e3eba8451e
Paul Bomba <paul@paulbomba.com>
2018-06-04 00:25:10 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+
+
 def reformat_languages(languages)
-  # your code here
-end
+newHash = {}
+
+  languages.each do |family, language|
+    language.each do |lang, type|
+      newHash[lang] = type
+    end
+  end
+styleArray = []
+  languages.each do |family, language|
+    language.each do |lang, type|
+      styleArray << family
+    end
+  end
+
+
+
+puts newHash
+
